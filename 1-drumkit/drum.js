f791655b12d51714cbe338e770078132fa0b4e7b
Hayato Clarke <hayatoclarke@gmail.com>
2020-09-07 14:01:32 +0900

    javascript added to find key and audio

--- 1-drumkit/drum.js
+++ 1-drumkit/drum.js
@@ -1,1 +1,10 @@
 console.log("hello!");
+
+const playSound = (e) => {
+  audio = this.document.querySelector(`audio[data-key="${e['keyCode']}"]`);
+  key = this.document.querySelector(`.drum-key[data-key="${e['keyCode']}"]`);
+  key.classList.add('playing');
+  console.log(audio);
+};
+
+window.addEventListener('keydown', playSound)

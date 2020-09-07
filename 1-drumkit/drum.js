d9b2eb51808e202c41f97ed615c99e5e3ffdcc46
Hayato Clarke <hayatoclarke@gmail.com>
2020-09-07 14:23:11 +0900

    struggle with removing clsas after adding it

--- 1-drumkit/drum.js
+++ 1-drumkit/drum.js
@@ -1,10 +1,18 @@
 console.log("hello!");
 
+const removePlaying = (key) => {
+  key.classList.remove('playing');
+}
+
 const playSound = (e) => {
   audio = this.document.querySelector(`audio[data-key="${e['keyCode']}"]`);
   key = this.document.querySelector(`.drum-key[data-key="${e['keyCode']}"]`);
+  if (!audio) { return }
   key.classList.add('playing');
-  console.log(audio);
+  audio.currentTime = 0;
+  audio.play();
+  setTimeout(1000);
+  key.classList.remove('playing');
 };
 
 window.addEventListener('keydown', playSound)

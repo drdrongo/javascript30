2837a30cc5881ce136f53df14bb119618e1a36c3
Hayato Clarke <hayatoclarke@gmail.com>
2020-09-08 11:33:07 +0900

    refactored javascript

--- 2-clock/clock.js
+++ 2-clock/clock.js
@@ -1,52 +1,61 @@
-setInterval(console.log('farts'), 1000);
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const secondHand = document.querySelector('.second-hand');
 
-const changeHands = (hoursDegrees, minutesDegrees, secondsDegrees) => {
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+const moveHands = (degrees) => {
+  hourHand.style.transform = `rotate(${degrees["hoursDegrees"]}deg)`;
+  minuteHand.style.transform = `rotate(${degrees["minutesDegrees"]}deg)`;
+  secondHand.style.transform = `rotate(${degrees["secondsDegrees"]}deg)`;
 }
 
-const removeSmoothness = (hoursDegrees, minutesDegrees, secondsDegrees) => {
-  if (hoursDegrees == 330) {
+const removeSmoothness = (degrees) => {
+  if (degrees["hoursDegrees"] == 330) {
     hourHand.classList.remove('smooth-movement');
   }
-  if (minutesDegrees == 354) {
+  if (degrees["minutesDegrees"] == 354) {
     minuteHand.classList.remove('smooth-movement');
   }
-  if (secondsDegrees == 354) {
+  if (degrees["secondsDegrees"] == 354) {
     secondHand.classList.remove('smooth-movement');
   }
 }
 
-const findTime = () => {
-  const date = new Date();
-
-  const seconds = date.getSeconds();
-  const secondsDegrees = (((seconds / 60) * 360) + 90) % 360;
-
-  const hours = date.getHours();
-  const hoursDegrees = ((((hours % 12) / 12) * 360) + 90) % 360;
-
-  const minutes = date.getMinutes();
-  const minutesDegrees = (((minutes / 60) * 360) + 90) % 360;
-
-  changeHands(hoursDegrees, minutesDegrees, secondsDegrees);
-  removeSmoothness(hoursDegrees, minutesDegrees, secondsDegrees)
-
-  
-  
-  if (secondsDegrees == 6) {
-    secondHand.classList.add('smooth-movement');
-  }
-  if (minutesDegrees == 6) {
-    minuteHand.classList.add('smooth-movement');
-  }
-  if (hoursDegrees == 30) {
+const addSmoothness = (degrees) => {
+  if (degrees["hoursDegrees"] == 30) {
     hourHand.classList.add('smooth-movement');
   }
+  if (degrees["minutesDegrees"] == 6) {
+    minuteHand.classList.add('smooth-movement');
+  }
+  if (degrees["secondsDegrees"] == 6) {
+    secondHand.classList.add('smooth-movement');
+  }
+}
+
+const getTimes = () => {
+  const date = new Date();
+  const times = {};
+  times["hours"] = date.getHours();
+  times["minutes"] = date.getMinutes();
+  times["seconds"] = date.getSeconds();
+  return times;
+}
+
+const getDegrees = (times) => {
+  const degrees = {};
+  degrees["hoursDegrees"] = ((((times["hours"] % 12) / 12) * 360) + 90) % 360;
+  degrees["minutesDegrees"] = (((times["minutes"] / 60) * 360) + 90) % 360;
+  degrees["secondsDegrees"] = (((times["seconds"] / 60) * 360) + 90) % 360;
+  return degrees;
+}
+
+const findTime = () => {
+  const times = getTimes();
+  const degrees = getDegrees(times);
+
+  moveHands(degrees);
+  removeSmoothness(degrees)
+  addSmoothness(degrees)
 }
 
 setInterval(findTime, 1000);
